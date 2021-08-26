1c6af098f1bd2856e5b38b669541aec0185c3642
Gabriel Ekodo <ekodogabriel@outlook.com>
2021-08-26 14:39:04 +0000

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main()
 {
     
-    puts("\"Programming is like building a multilingual puzzle"); //writing the string to stdout
+    puts("\"Programming is like building a multilingual puzzle");
      
     return 0;
 }
