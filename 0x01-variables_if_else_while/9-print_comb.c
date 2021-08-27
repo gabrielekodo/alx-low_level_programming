bc882955c083ef5d0c84576bebefd7274c3bc2c6
Gabriel Ekodo <ekodogabriel@outlook.com>
2021-08-27 13:42:39 +0000

    2-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints all single-digit combinations
+ * Return: 0 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(i);
+if (x != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
