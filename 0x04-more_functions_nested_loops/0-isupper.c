1f09676661fafc93a3d22bb5adb96ad45c2843cc
Gabriel Ekodo <ekodogabriel@outlook.com>
2021-09-02 10:47:08 +0000

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+
+
+
+#include <stdio.h>
+int main(void){
+	char var_name[20];
+	printf("May I know your name? ");
+	scanf("%s", var_name);
+	printf("Hello %s, let us be friends.\n", var_name);
+	return 0;
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - returns the product of a and b
+ * @a: 1st argument
+ * @b: 2nd argument
+ *
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+
+return (a * y);
+
+}
+
