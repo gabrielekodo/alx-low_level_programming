3d08d76d90dc598fd1f8110db1cddeaaafaf8640
Gabriel Ekodo <ekodogabriel@outlook.com>
2021-09-17 09:58:51 +0000

    who am i

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,12 @@
  *
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-argc;
-printf("%s\n", argv[0]);
+	while (argc--)
+	{
+printf("%s\n", *argv++);
+
+	}
 return (0);
 }
