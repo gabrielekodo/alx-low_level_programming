81a2f65b962ee49d5c8ef04a037d5efd8b73070e
Gabriel Ekodo <ekodogabriel@outlook.com>
2021-09-17 09:45:23 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - starting point
+ *@argc: number of arguments
+ *@argv: arguments
+ *
+ *
+ *Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+argc;
+printf("%s\n", argv[0]);
+return (0);
+}
